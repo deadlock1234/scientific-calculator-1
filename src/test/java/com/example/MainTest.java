8dd42657ba4f7191be8a1d5e2335094bb58db2aa
seeker812 <sumitsinghpaliya9087@gmail.com>
2024-02-21 01:34:46 +0530

    Final all case test done

--- src/test/java/com/example/MainTest.java
+++ src/test/java/com/example/MainTest.java
@@ -1,10 +1,9 @@
 package com.example;
-import com.example.Main;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class MainTest{
+public class MainTest {
     Main main = new Main();
     double delta = 0.001;
 
